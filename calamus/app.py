9c6222c1e14bfde68c5c7bce5f66ffa1124be116
mray271 <24377848+mray271@users.noreply.github.com>
2026-07-12 15:54:28 -0700

    LNT

--- calamus/app.py
+++ calamus/app.py
@@ -11,7 +11,7 @@ import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
-from gi.repository import Adw, GLib, Gio
+from gi.repository import Adw, Gio, GLib
 
 from calamus.theme import ThemeManager
 from calamus.window import CalamusWindow
